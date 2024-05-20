d1a73ca133a6ab08c30dbc39fcb5c145a1034e8b
UmairAnsari1 <ansariumairislam21@gmail.com>
2024-05-20 14:39:35 +0530

    Initial Commit

---
+++ Hot Prospects/Hot_ProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hot_ProspectsApp.swift
+//  Hot Prospects
+//
+//  Created by Umair on 20/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Hot_ProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
