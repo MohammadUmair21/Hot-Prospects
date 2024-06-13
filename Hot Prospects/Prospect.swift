465efe0ea2db4c9ac99a9623770027f7fac41d21
UmairAnsari1 <ansariumairislam21@gmail.com>
2024-06-13 15:08:21 +0530

    Hot Prospect source code

---
+++ Hot Prospects/Prospect.swift
@@ -0,0 +1,8 @@
+//
+//  Prospect.swift
+//  Hot Prospects
+//
+//  Created by Umair on 20/05/24.
+//
+
+import Foundation
---
+++ Hot Prospects/ProspectsView.swift
@@ -0,0 +1,18 @@
+//
+//  ProspectsView.swift
+//  Hot Prospects
+//
+//  Created by Umair on 20/05/24.
+//
+
+import SwiftUI
+
+struct ProspectsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProspectsView()
+}
